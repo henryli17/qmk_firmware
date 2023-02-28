b839cc6b53c0a66055c78bd9be5e05ce9105d7a4
Henry Li <henry_li@web.de>
2023-02-28 22:34:39 +0000

    Brightness keys

--- keyboards/ymdk/ym68/keymaps/henryli17/keymap.c
+++ keyboards/ymdk/ym68/keymaps/henryli17/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,   KC_LGUI,   KC_LALT,                       KC_SPC,                              KC_RALT,  MO(1),  KC_RCTL, KC_LEFT, KC_DOWN, KC_RIGHT
     ),
     [1] = LAYOUT_all(
-        QK_BOOT,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_F23, KC_F23, KC_F24,
+        QK_BOOT,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_F18, KC_BRIGHTNESS_DOWN, KC_BRIGHTNESS_UP,
         _______,     _______, _______,  KC_END, _______, _______, _______, _______, KC_INS, _______, KC_PAUSE, KC_PGDN, KC_PGUP, _______,      _______,
         _______,       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MUTE,             _______,
         _______,   _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______,       KC_VOLU, _______,
