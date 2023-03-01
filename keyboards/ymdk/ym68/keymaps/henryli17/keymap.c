88e1daecf14d9a219f0e7296e2bf52a4513f9242
Henry Li <henry_li@web.de>
2023-03-01 17:24:54 +0000

    Update layout

--- keyboards/ymdk/ym68/keymaps/henryli17/keymap.c
+++ keyboards/ymdk/ym68/keymaps/henryli17/keymap.c
@@ -17,28 +17,28 @@
 #include QMK_KEYBOARD_H
 
 enum {
-  TD_DTAP_NEXT,
-  TD_DTAP_PREV
+  DTAP_MNXT,
+  DTAP_MPRV
 };
 
 tap_dance_action_t tap_dance_actions[] = {
-    [TD_DTAP_NEXT] = ACTION_TAP_DANCE_DOUBLE(KC_NO, KC_MNXT),
-    [TD_DTAP_PREV] = ACTION_TAP_DANCE_DOUBLE(KC_NO, KC_MPRV)
+    [DTAP_MNXT] = ACTION_TAP_DANCE_DOUBLE(KC_NO, KC_MNXT),
+    [DTAP_MPRV] = ACTION_TAP_DANCE_DOUBLE(KC_NO, KC_MPRV)
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_all(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_GRV,  KC_BSPC, KC_GRV,
         KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,      KC_DEL,
-        KC_CAPS,       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,              TD(TD_DTAP_NEXT),
-        KC_LSFT,   KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,        KC_UP,   TD(TD_DTAP_PREV),
+        KC_CAPS,       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,              TD(DTAP_MNXT),
+        KC_LSFT,   KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,        KC_UP,   TD(DTAP_MPRV),
         KC_LCTL,   KC_LGUI,   KC_LALT,                       KC_SPC,                              KC_RALT,  MO(1),  KC_RCTL, KC_LEFT, KC_DOWN, KC_RIGHT
     ),
     [1] = LAYOUT_all(
-        QK_BOOT,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_F18, KC_BRIGHTNESS_DOWN, KC_BRIGHTNESS_UP,
+        QK_BOOT,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_F18, KC_F18,  KC_F19,
         _______,     _______, _______,  KC_END, _______, _______, _______, _______, KC_INS, _______, KC_PAUSE, KC_PGDN, KC_PGUP, _______,      _______,
         _______,       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MUTE,             _______,
         _______,   _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______,       KC_VOLU, _______,
-        _______,   _______,   _______,                      KC_MPLY,                              _______, _______, _______, _______, KC_VOLD, _______
+        _______,   _______,   _______,                      KC_MPLY,                              _______, _______, _______, KC_BRID, KC_VOLD, KC_BRIU
     ),
 };
